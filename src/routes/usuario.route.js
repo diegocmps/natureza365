0afbcdafa7bea542464ef87f8a6d8e12d8841804
diegocmps <diegobarreiros@hotmail.com>
2024-10-31 19:10:03 -0300

    ajuste nos códigos de atualização de usuário e rotas

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -1,18 +1,16 @@
-const { Router } = require("express");
-const UsuarioController = require("../controllers/UsuarioController");
-const Yup = require('yup');
-const { auth } = require("../middleware/auth");
-const validarUsuario = require("../middleware/validarUsuario");
-const usuarioSchema = require("../schemas/usuarioSchema");
+const { Router } = require('express');
+const UsuarioController = require('../controllers/UsuarioController');
+const { auth } = require('../middleware/auth');
+
+const usuarioRoutes = Router();
 
 
-const usuarioRoutes = Router()
+usuarioRoutes.post('/', UsuarioController.cadastrar);  
+usuarioRoutes.get('/:id', auth, UsuarioController.buscarPorId); 
+usuarioRoutes.get('/', auth, UsuarioController.buscarTodos);  
+usuarioRoutes.delete('/:id', auth, UsuarioController.excluir); 
+usuarioRoutes.put('/:id', auth, UsuarioController.editar);
 
 
-usuarioRoutes.post('/', validarUsuario(usuarioSchema), UsuarioController.cadastrar)
-usuarioRoutes.put('/:id', auth, validarUsuario(usuarioSchema), UsuarioController.atualizar)
-usuarioRoutes.delete('/:id', auth, UsuarioController.deletar)
 
-module.exports = usuarioRoutes
-
-
+module.exports = usuarioRoutes;
