bae25d905a7b7458855541af894678666e85a3f2
diegocmps <diegobarreiros@hotmail.com>
2024-11-01 11:23:42 -0300

    Correção de status de usuário isLogged ao realizar Login e Logout

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const LoginController = require("../controllers/LoginController");
+const { auth } = require("../middleware/auth");
+
 
 const loginRoutes = new Router()
 
 loginRoutes.post('/', LoginController.login)
+loginRoutes.post('/logout', auth, LoginController.logout)
 
 module.exports = loginRoutes
