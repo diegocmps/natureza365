4a0ed57f2fcebbdf90bd7db7a2631c26a4626f82
diegocmps <diegobarreiros@hotmail.com>
2024-11-01 16:41:26 -0300

    ajustes para configuração do postgreSQL

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -6,7 +6,7 @@ module.exports = {
   host: process.env.DB_HOST, //Qual servidor está utilizando;
   username: process.env.DB_USERNAME, //Qual o nome do seu usuário no postgres;
   password: process.env.DB_PASSWORD, //Qual a senha do seu usuário no postgres;
-  database: process.env.DATABASE, //Qual o nome do seu database no postgres;
+  database: process.env.DB_DATABASE, //Qual o nome do seu database no postgres;
   port: process.env.DB_PORT, //Qual porta do seu postgres (Normalmente é a 5432);
   dialectOptions: {
     ssl: {
