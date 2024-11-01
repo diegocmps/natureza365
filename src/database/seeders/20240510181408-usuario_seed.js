4cbf939f8c8b16d71782e6c22dcc7c2d5e9cb1af
diegocmps <diegobarreiros@hotmail.com>
2024-11-01 14:04:24 -0300

    inclusão de novas seeds, com dados atualizados

--- src/database/seeders/20240510181408-usuario_seed.js
+++ src/database/seeders/20240510181408-usuario_seed.js
@@ -1,38 +1,100 @@
-'use strict';
-
+const bcrypt = require('bcryptjs');
 const Usuario = require('../../models/Usuario');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const usuarios = [
-      {
-        nome: 'Joao das Flores',
-        sexo: 'masculino',
-        cpf: '12345678911',
-        endereco: 'Rua das Flores, 52 - Canasvieiras - Florianópolis/SC',
-        email: 'joaodasflores@email.com',
-        senha: '12345678',
-        data_nascimento: '1989-07-26'
-      },
-      {
-        nome: 'Maria das Flores',
-        sexo: 'feminino',
-        cpf: '98765432198',
-        endereco: 'Rua das Flores, 52 - Canasvieiras - Florianópolis/SC',
-        email: 'mariadasflores@email.com',
-        senha: '12345678',
-        data_nascimento: '1989-07-26'
-      }
-    ]
+    up: async (QueryInterface, Sequelize) => {
+        const saltRounds = 10;
 
-    for (const usuario of usuarios) {
-      await Usuario.create(usuario);
+        await Usuario.bulkCreate([
+            {
+                nome: "Raphaela Assis",
+                email: "rapha.exemplo@hotmail.com",
+                cpf: "01236523879",
+                sexo: "Feminino",
+                senha: await bcrypt.hash("rapha123", saltRounds),
+                data_nascimento: "1989-01-05",
+                cep: "88130-000",
+                rua: "Rua A",
+                numero: 123,
+                complemento: "Apto 1",
+                bairro: "Campeche",
+                cidade: "Florianópolis",
+                estado: "SC"
+            },
+            {
+                nome: "Kauana Araujo",
+                email: "kaka.exemplo@hotmail.com",
+                cpf: "14151917987",
+                sexo: "Feminino",
+                senha: await bcrypt.hash("kaka123", saltRounds),
+                data_nascimento: "2002-12-09",
+                cep: "88060-000",
+                rua: "Rua B",
+                numero: 456,
+                complemento: "Casa",
+                bairro: "Ratones",
+                cidade: "Florianópolis",
+                estado: "SC"
+            },
+            {
+                nome: "Tiago Araujo",
+                email: "tiago.exemplo@hotmail.com",
+                cpf: "78945632115",
+                sexo: "Masculino",
+                senha: await bcrypt.hash("tiago123", saltRounds),
+                data_nascimento: "1992-05-10",
+                cep: "88120-000",
+                rua: "Rua C",
+                numero: 789,
+                complemento: "",
+                bairro: "Arambaré",
+                cidade: "Florianópolis",
+                estado: "SC"
+            },
+            {
+                nome: "Maria Eduarda",
+                email: "duda.exemplo@hotmail.com",
+                cpf: "52687413699",
+                sexo: "Feminino",
+                senha: await bcrypt.hash("duda123", saltRounds),
+                data_nascimento: "2002-09-19",
+                cep: "88110-000",
+                rua: "Rua D",
+                numero: 321,
+                complemento: "Bloco B",
+                bairro: "Camaquã",
+                cidade: "Florianópolis",
+                estado: "SC"
+            },
+            {
+                nome: "Mario Alberto",
+                email: "mario.exemplo@hotmail.com",
+                cpf: "52687413698",
+                sexo: "Masculino",
+                senha: await bcrypt.hash("mario123", saltRounds),
+                data_nascimento: "2002-09-19",
+                cep: "88110-000",
+                rua: "Rua D",
+                numero: 321,
+                complemento: "Bloco B",
+                bairro: "Camaquã",
+                cidade: "Florianópolis",
+                estado: "SC"
+            }
+        ]);
+    },
+
+    down: async (QueryInterface, Sequelize) => {
+        await Usuario.destroy({
+            where: {
+                email: [
+                    "rapha.exemplo@hotmail.com",
+                    "kaka.exemplo@hotmail.com",
+                    "tiago.exemplo@hotmail.com",
+                    "duda.exemplo@hotmail.com",
+                    "mario.exemplo@hotmail.com"
+                ]
+            }
+        });
     }
-  },
-
-  down: async(queryInterface, Sequelize) => {
-    await Usuario.destroy({email: ['joaodasflores@email.com', 'mariadasflores@email.com']});
-  }
 };
-
--- src/database/seeders/20240510192802-local_seed.js
+++ src/database/seeders/20240510192802-local_seed.js
@@ -1,30 +1,89 @@
-'use strict';
 
+const { QueryInterface, Sequelize } = require("sequelize");
 const Local = require('../../models/Local');
 
-
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const locais = [
+    await Local.bulkCreate([
       {
-        usuario_id: 1,
-        nome_local: 'Trilha da Lagoinha do Leste',
-        descricao: 'A Praia da Lagoinha do Leste é um dos paraísos mais preservados do sul de Florianópolis...',
-        cep: '88067079',
-        localidade: '88067-079, Pântano do Sul, Florianópolis, Região Geográfica Imediata de Florianópolis, Região Geográfica Intermediária de Florianópolis, Santa Catarina, Região Sul, Brasil',
-        coord_geo: 'https://www.google.com/maps/search/?api=1&query=-27.7790083,-48.506409',
+        nome: 'Trilha Morro das Aranhas',
+        descricao: 'Trilha de aproximadamente 45 min de subida, com uma vista para as praias do Santinho, Moçambique e Ingleses',
+        cep: '88058-700',
+        rua: 'Estrada Ver. Onildo Lemos',
+        bairro: 'Ingleses do Rio Vermelho',
+        cidade: 'Florianópolis',
+        estado: 'SC',
+        usuarioId: 1,
+        latitude: -27.468279851664402,
+        longitude: -48.375781422036475, 
+      },
+      {
+
+        
+        nome: 'Trilha Lagoinha do Leste',
+        descricao: 'Praia, costões, lagoa, cachoeira e mata nativa. Esses ingredientes estão todos juntos na Lagoinha do Leste, que esconde seu encanto entre os morros do Sul da Ilha de Santa Catarina.',
+        cep: '88135-222',
+        rua: 'R. Manoel Pedro Oliveira',
+        bairro: 'Pântano do Sul',
+        cidade: 'Florianópolis',
+        estado: 'SC',
+        usuarioId: 2,
+        latitude: -27.77896039728563,
+        longitude: -48.506183624679764
+      },
+      {
+        nome: 'Trilha Soldados de Sebold',
+        descricao: 'Essas espetaculares formações rochosas de arenito botucatu são um verdadeiro tesouro nas encostas da Serra Geral. Com imponentes 90 metros de altura cada, essas rochas se destacam lado a lado, evocando a imagem de quatro soldados alinhados em guarda nas montanhas.',
+        cep: '88450-000',
+        rua: 'Parque Soldados de Sebold',
+        bairro: 'Soldados de Sebold',
+        cidade: 'Alfredo Wagner',
+        estado: 'SC',
+        usuarioId: 3,
+        latitude: -27.83782501454029,
+        longitude: -49.28194209386551
+
+      
+      },
+      {    
+        nome: 'Trilha Costeira do Zimbros',
+        descricao: 'Com início na Praia do Cantinho de Zimbros, a trilha tem dificuldade moderada por causa da longa extensão, e tem uma duração de aproximadamente 6 horas. No caminho, você encontra as seguintes praias: Praia do Cardoso, Praia do Basílio, Praia da Lagoa, Praia do Casqueiro, Praia do Pasto, Praia do Lau/Hermínio, Praia Triste, Praia do Mauri, Praia da Santa e Praia Vermelha.',
+        cep: '88215-000',
+        rua: 'R. Rio Piratini',
+        bairro: 'Zimbros',
+        cidade: 'Bombinhas',
+        estado: 'SC',
+        usuarioId: 4,
+        latitude: -27.181949145957496,
+        longitude: -48.54178575940444
+      },
+      {  
+        nome: 'Pico do Cambirela',
+        descricao: 'A Trilha do Cambirela é uma das trilhas que mais atrai os aventureiros da região. É uma trilha cansativa e considerada muito difícil de ser percorrida devido ao nível de preparo físico necessário. O Morro do Cambirela (922 m) é uma montanha situada no maciço do mesmo nome e fica localizado no Município de Palhoça, próximo de Florianópolis. Está situado na Serra do Tabuleiro e está dentro do território do Parque Estadual da Serra do Tabuleiro(PEST)',
+        cep: '88138-788',
+        rua: 'BR-101, Km 222',
+        bairro: 'Enseada do Brito',
+        cidade: 'Palhoça',
+        estado: 'SC',
+        usuarioId: 4,
+        latitude: -27.710134784512377,
+        longitude: -48.65071782742521
+      },
+      {    
+        nome: 'Trilha de Naufragados',
+        descricao: 'A Praia de Naufragados, no extremos sul da Ilha, é uma praia isolada com acesso apenas por trilha ou por barco. A trilha mais usada para chegar lá, sai da Caieira da Barra do Sul (no extremo sul do Ribeirão da Ilha), é um caminho fácil, aberto e super conhecido. Mas pouca gente sabe que também dá para chegar na Praia de Naufragados a partir da Praia da Solidão.',
+        cep: '88064-002',
+        rua: 'Rod. Baldicero Filomeno, 20409',
+        bairro: 'Caieira',
+        cidade: 'Florianópolis',
+        estado: 'SC',
+        usuarioId: 4,
+        latitude: -27.816341955118446,
+        longitude: -48.56085523453938, 
       }
-    ]
-
-    for (const local of locais) {
-      await Local.create(local);
-    }
+    ]);
   },
-
-  down: async(queryInterface, Sequelize) => {
-    await Local.destroy({where: {usuario_id: [1]}});
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('locais', null, {});
   }
-}
-
+};
