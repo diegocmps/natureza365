7dc560d98352472572f22ff5e6987ac33bad13a9
diegocmps <diegobarreiros@hotmail.com>
2025-08-11 14:48:46 -0300

    preparação para deploy

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,22 +1,24 @@
-const { config } = require('dotenv').config()
+const { config } = require("dotenv").config();
 
 module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, 
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false,
-      require: true
-    }
+      require: true,
+    },
   },
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000 // idle timeout in milliseconds
-  }
+    idle: 10000, // idle timeout in milliseconds
+  },
 };
+
+//teste
